2d64bd853d647d0a151f53c2a548bba955afeeba
NodeNebula <node.nebula@gmail.com>
2023-11-29 22:07:10 +0100

    Starting layout

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="menu">
+        <a class="home-btn" href=""></a>
+        <a class="github-btn" href=""></a>
+        <a class="about-btn" href=""></a>
+        <a class="contact-btn" href=""></a>
+        <a class="" href=""></a>
+    </div>
+
+    <div class="skills"></div>
+    <div class="devider"></div>
+    <div class="info">
+        <div class="about-me"></div>
+        <div class="contact"></div>
+    </div>
+</body>
+</html>
